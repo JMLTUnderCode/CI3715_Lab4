b6eb3988d59a2c44426ac9ac458c7727ee61990d
Junior Miguel Lara Torres <45317638+JMLTUnderCode@users.noreply.github.com>
2024-11-08 13:42:35 -0400

    Inclusión de Black a la Suite de pruebas (#2)
    * Inclusión de pasos a la suite de pruebas del proyecto. Inclusión de Black para formato de codigo.
    
    * Modicación realizada por la aplicación de Black para el formato de código al proyecto.

--- fib.py
+++ fib.py
@@ -5,16 +5,19 @@ El número cero en la secuencia de Fibonacci es 0. El primer número es 1
 Los números negativos no son aceptados
 """
 
+
 def fibonacci(position):
-  # Error en caso de valores negativos.
-  if(position < 0): raise ValueError("Invalid input")
+    # Error en caso de valores negativos.
+    if position < 0:
+        raise ValueError("Invalid input")
 
-  # (INCLUIR) caso base cuando tenemos valor 0.
-  if(position == 0): return 0
+    # (INCLUIR) caso base cuando tenemos valor 0.
+    if position == 0:
+        return 0
 
-  # Caso base cuando tenemos valor 1 y 2.
-  if(position == 1 or position == 2): return 1
-  
-  # Caso recursivo.
-  return fibonacci(position - 1) + fibonacci(position - 2)
-  
+    # Caso base cuando tenemos valor 1 y 2.
+    if position == 1 or position == 2:
+        return 1
+
+    # Caso recursivo.
+    return fibonacci(position - 1) + fibonacci(position - 2)
